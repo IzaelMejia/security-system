00956dcb683964838280afcc43e27f69fd61d7b2
IzaelMejia <izaelmejiaa@gmail.com>
2023-11-06 21:41:35 -0600

    Hora fin no menor a Inicio *Correxión

--- scripts/fechaReporte.js
+++ scripts/fechaReporte.js
@@ -29,14 +29,22 @@ document.addEventListener('DOMContentLoaded', function() {
         var horaFin = document.getElementById("horaFin").value;
 
         if (rangoFechas && horaInicio && horaFin) {
-            var rangoSeleccionado = "Rango de fechas: " + rangoFechas + "<br>";
-            rangoSeleccionado += "Hora de inicio: " + horaInicio + "<br>";
-            rangoSeleccionado += "Hora de fin: " + horaFin;
-            
-            document.getElementById("rangoSeleccionado").innerHTML = rangoSeleccionado;
-            document.getElementById("rangoFechas").value = "";
-            document.getElementById("horaInicio").value = "";
-            document.getElementById("horaFin").value = "";
+            // Comprueba si la hora de fin es mayor o igual que la hora de inicio
+            var horaInicioObj = new Date("2000-01-01 " + horaInicio);
+            var horaFinObj = new Date("2000-01-01 " + horaFin);
+
+            if (horaFinObj >= horaInicioObj) {
+                var rangoSeleccionado = "Rango de fechas: " + rangoFechas + "<br>";
+                rangoSeleccionado += "Hora de inicio: " + horaInicio + "<br>";
+                rangoSeleccionado += "Hora de fin: " + horaFin;
+
+                document.getElementById("rangoSeleccionado").innerHTML = rangoSeleccionado;
+                document.getElementById("rangoFechas").value = "";
+                document.getElementById("horaInicio").value = "";
+                document.getElementById("horaFin").value = "";
+            } else {
+                document.getElementById("rangoSeleccionado").textContent = "La hora de fin no puede ser menor que la hora de inicio.";
+            }
         } else {
             document.getElementById("rangoSeleccionado").textContent = "Por favor, completa todos los campos.";
         }
