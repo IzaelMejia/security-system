17610c52deedfd7afcf6425a45e94301721e9ff1
IzaelMejia <izaelmejiaa@gmail.com>
2023-11-15 23:20:45 -0600

    mostrar excel de aceurdo a datos en la bd

--- scripts/fechaReporte.js
+++ scripts/fechaReporte.js
@@ -50,3 +50,57 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+document.getElementById("mostrarExcel").addEventListener("click", function() {
+    var rangoFechas = document.getElementById("rangoFechas").value;
+    var horaInicio = document.getElementById("horaInicio").value;
+    var horaFin = document.getElementById("horaFin").value;
+
+    if (rangoFechas && horaInicio && horaFin) {
+        // Envía una solicitud al servidor para obtener datos de la base de datos
+        axios.post('/generarExcel', { rangoFechas, horaInicio, horaFin }, { responseType: 'blob' })
+            .then(function(response) {
+                // Crea un objeto Blob desde la respuesta
+                var blob = new Blob([response.data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+
+                // Crea un enlace temporal y simula un clic para iniciar la descarga
+                var link = document.createElement('a');
+                link.href = window.URL.createObjectURL(blob);
+                link.download = 'reporte.xlsx';
+                
+                // Agrega el enlace al DOM y simula un clic para iniciar la descarga
+                document.body.appendChild(link);
+                link.click();
+
+                // Elimina el enlace del DOM después de la descarga
+                document.body.removeChild(link);
+            })
+            .catch(function(error) {
+                console.error('Error al obtener datos de la base de datos:', error);
+            });
+    } else {
+        document.getElementById("rangoSeleccionado").textContent = "Por favor, completa todos los campos.";
+    }
+});
+
+document.getElementById("mostrarPDF").addEventListener("click", function () {
+    var rangoFechas = document.getElementById("rangoFechas").value;
+    var horaInicio = document.getElementById("horaInicio").value;
+    var horaFin = document.getElementById("horaFin").value;
+
+    if (rangoFechas && horaInicio && horaFin) {
+        // Envía una solicitud al servidor para obtener datos de la base de datos
+        axios.post('/generarPDF', { rangoFechas, horaInicio, horaFin })
+            .then(function (response) {
+                console.error('PDF creado exitosamente');
+            })
+            .catch(function (error) {
+                console.error('Error al obtener datos de la base de datos:', error);
+            });
+    } else {
+        document.getElementById("rangoSeleccionado").textContent = "Por favor, completa todos los campos.";
+    }
+});
+
+
+  
--- scripts/server.js
+++ scripts/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
+const excel = require('exceljs');
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, '../pages')));
@@ -35,10 +36,94 @@ app.get('/registro', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'pages', 'register.html'));
 });
 
+app.post('/generarExcel', (req, res) => {
+  const rangoFechas = req.body.rangoFechas;
+  const horaInicio = req.body.horaInicio;
+  const horaFin = req.body.horaFin;
+  
+  const partes = rangoFechas.split(" ");
+  const fechaInicial = partes[0];
+  const fechaFinal = partes[2];
+
+  connection.query(
+    'SELECT con_general, con_fecha, con_hora FROM contadorGeneral WHERE con_fecha BETWEEN ? AND ? AND con_hora BETWEEN ? AND ?;',
+    [fechaInicial, fechaFinal, horaInicio, horaFin],
+    (err, results) => {
+      if (err) {
+        console.error('Error al consultar la base de datos:', err);
+        res.status(500).send('Error al consultar la base de datos');
+      } else {
+        const workbook = new excel.Workbook();
+        const worksheet = workbook.addWorksheet('Reporte');
+
+        worksheet.columns = [
+          { header: 'Contador', key: 'con_general', width: 15 },
+          { header: 'Fecha', key: 'con_fecha', width: 15 },
+          { header: 'Hora', key: 'con_hora', width: 15 },
+        ];
+
+        results.forEach((row) => {
+          worksheet.addRow(row);
+        });
+
+        // Convierte el libro de trabajo a un flujo de datos y envíalo como respuesta
+        workbook.xlsx.write(res, { type: 'stream' })
+          .then(() => {
+            console.log('Archivo Excel enviado con éxito');
+          })
+          .catch((error) => {
+            console.error('Error al enviar el archivo Excel:', error);
+            res.status(500).send('Error al enviar el archivo Excel');
+          });
+      }
+    }
+  );
+  
+  console.log('Fecha Inicio:', fechaInicial);
+  console.log('Fecha Final:', fechaFinal);
+  console.log('Rango de Fecha seleccionada:', rangoFechas);
+  console.log('Hora de inicio:', horaInicio);
+  console.log('Hora de fin:', horaFin);
+});
+
+// ... (código existente)
+
+app.post('/generarPDF', async (req, res) => {
+  const rangoFechas = req.body.rangoFechas;
+  const horaInicio = req.body.horaInicio;
+  const horaFin = req.body.horaFin;
+
+  const partes = rangoFechas.split(" ");
+  const fechaInicial = partes[0];
+  const fechaFinal = partes[2];
+
+  // Realiza la consulta a la base de datos utilizando la conexión MySQL
+  connection.query(
+    'SELECT con_general, con_fecha, con_hora FROM contadorGeneral WHERE con_fecha BETWEEN ? AND ? AND con_hora BETWEEN ? AND ?;',
+    [fechaInicial, fechaFinal, horaInicio, horaFin],
+    async (err, results) => {
+      if (err) {
+        console.error('Error al consultar la base de datos:', err);
+        res.status(500).send('Error al consultar la base de datos');
+      } else {
+
+      }
+    }
+  );
+
+  console.log('Fecha Inicio:', fechaInicial);
+  console.log('Fecha Final:', fechaFinal);
+  console.log('Rango de Fecha seleccionada:', rangoFechas);
+  console.log('Hora de inicio:', horaInicio);
+  console.log('Hora de fin:', horaFin);
+});
+
+
 // Correr el servidor web
 app.listen(port, () => {
   console.log(`Servidor web en ejecución en http://localhost:${port}`);
 });
+
 
 // Cerrar la conexión a MySQL cuando se cierre el servidor web
 process.on('SIGINT', () => {
